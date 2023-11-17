ecdeebdcba98ce548fd3b596120d891674f8ea2c
BMDK9 <qnrakdlzm77@naver.com>
2023-11-17 11:03:29 +0900

    first commit

---
+++ src/main/java/com/example/homework2_todolist/HomeWork2ToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework2_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork2ToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork2ToDoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework2_todolist/HomeWork2ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork2ToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
