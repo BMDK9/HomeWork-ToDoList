b12aa9c2a922e49024c6898ca8a4a7da66571d70
BMDK9 <qnrakdlzm77@naver.com>
2023-11-20 11:25:02 +0900

    feat : 댓글기능 구현 중

--- src/main/java/com/sparta/homework2_todolist/controller/CommentController.java
+++ src/main/java/com/sparta/homework2_todolist/controller/CommentController.java
@@ -1,15 +1,21 @@
-package com.sparta.homework2_todolist.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+//package com.sparta.homework2_todolist.controller;
+//
+//import com.sparta.homework2_todolist.repository.CardRepository;
+//import com.sparta.homework2_todolist.security.UserDetailsImpl;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.web.bind.annotation.*;
+//
 //@RestController
 //@RequestMapping("/api/comments")
 //@RequiredArgsConstructor
 //public class CommentController {
 //
 //    @PostMapping
-//
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public CommentResponseDto addComment(@RequestBody CommentRequestDto commentRequestDto,
+//                                         @AuthenticationPrincipal UserDetailsImpl userDetails,
+//                                         ) {
+//    }
 //}
