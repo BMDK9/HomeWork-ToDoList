6e68d3add706c594e60445b43ec773175d681f0d
BMDK9 <qnrakdlzm77@naver.com>
2023-12-02 21:42:10 +0900

    feat : Card service test 할일 카드 추가, 카드 삭제 구현

---
+++ src/test/java/com/sparta/homework2_todolist/domain/card/service/CardServiceTest.java
@@ -0,0 +1,86 @@
+package com.sparta.homework2_todolist.domain.card.service;
+
+import com.sparta.homework2_todolist.domain.card.dto.CardRequestDto;
+import com.sparta.homework2_todolist.domain.card.dto.CardResponseDto;
+import com.sparta.homework2_todolist.domain.card.entity.Card;
+import com.sparta.homework2_todolist.domain.card.repository.CardRepository;
+import com.sparta.homework2_todolist.domain.users.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class CardServiceTest {
+
+    private User user1, user2;
+
+    private Card card1, card2;
+
+    @Mock
+    private CardRepository cardRepository;
+
+    @InjectMocks
+    private CardService cardService;
+
+    @BeforeEach
+    void setUp() {
+        user1 = User.builder().userId(1L).username("성훈")
+            .build();
+
+        user2 = User.builder().userId(2L).username("김성훈")
+            .build();
+
+        card1 = Card.builder().title("할일 카드").contents("추가 테스트").user(user1)
+            .isDone(false).isHidden(false)
+            .build();
+
+        card2 = Card.builder().title("2222").contents("2222").user(user2)
+            .isDone(false).isHidden(false)
+            .build();
+    }
+
+    @DisplayName("할일 카드 추가 테스트")
+    @Test
+    void addToDo() {
+        // given
+        CardRequestDto cardRequestDto = CardRequestDto.builder()
+            .title("할일 카드").content("추가 테스트")
+            .build();
+
+        BDDMockito.given(cardRepository.save(any(Card.class))).willReturn(card1);
+
+        // when
+        CardResponseDto result = cardService.addToDo(cardRequestDto, user1);
+
+        // then
+        assertThat(result).extracting("Title", "Content").contains("할일 카드", "추가 테스트");
+
+//        assertEquals(cardRequestDto.getTitle(), result.getTitle());
+//        assertEquals(cardRequestDto.getContent(), result.getContent());
+    }
+
+    @DisplayName("카드 삭제 테스트")
+    @Test
+    void deleteCard() {
+        // given
+        BDDMockito.given(cardRepository.findById(anyLong())).willReturn(Optional.of(card2));
+
+        // when
+        cardService.deleteCard(1L, user2);
+
+        // then
+        Mockito.verify(cardRepository, Mockito.times(1)).delete(any(Card.class));
+    }
+}
